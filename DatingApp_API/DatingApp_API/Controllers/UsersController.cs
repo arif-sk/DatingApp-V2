0e1a32049285a982b7c4bfa0872cb56aea631e0a
arif-sk <arif.hossain177@gmail.com>
2022-01-30 01:35:44 +0600

    added angular projects

---
+++ DatingApp_API/DatingApp_API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using DatingApp_API.Data;
+using DatingApp_API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
