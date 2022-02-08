35dad49ffadfedf811a3f0a2bd4a8bc6b70d78c4
arif-sk <arif.hossain177@gmail.com>
2022-02-09 01:50:14 +0600

    changes

---
+++ DatingApp_API/DatingApp_API/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using DatingApp_API.Data;
+using DatingApp_API.DTOs;
+using DatingApp_API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DatingApp_API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext context;
+
+        public AccountController(DataContext context)
+        {
+            this.context = context;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> RegisterUser(RegisterDto register)
+        {
+            if (await UserExists(register.Username)) return BadRequest("Username is taken");
+            using var hmac = new HMACSHA512();
+            var user = new AppUser()
+            {
+                UserName = register.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(register.Password)),
+                PasswordSalt = hmac.Key
+            };
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto login)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(x => x.UserName.ToLower().Equals(login.Username.ToLower()));
+            if (user == null) return Unauthorized("Invalid User");
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(login.Password));
+            for(var i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+            return user;
+        }
+        private async Task<bool> UserExists(string username)
+        {
+            return await context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
+    }
+}
--- DatingApp_API/DatingApp_API/Data/Migrations/20220129195406_AddedPasswordField.Designer.cs
+++ DatingApp_API/DatingApp_API/Data/Migrations/20220129195406_AddedPasswordField.Designer.cs
@@ -4,6 +4,7 @@ using DatingApp_API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatingApp_API.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20220129195406_AddedPasswordField")]
+    partial class AddedPasswordField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
